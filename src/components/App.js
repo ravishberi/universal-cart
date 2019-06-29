61416318edbd45a74c77ab685a4db571f8fa78d1
jaskaranpreets7 <34167166+jaskaranpreets7@users.noreply.github.com>
2019-06-29 17:24:46 -0400

    created sub folders for styles and components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
