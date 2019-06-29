18fdf8bae65f66e0c26d7d75589c06e9f66c93d6
jaskaranpreets7 <34167166+jaskaranpreets7@users.noreply.github.com>
2019-06-29 17:26:26 -0400

    classComponent to functionalComponent/added hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,69 @@
-import React,{Component} from 'react';
-import '../components/Home.css';
+import React, { useState } from 'react';
+import '../styles/home.css';
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            formTextField: null,
-            formButton: null,
-        }
+function Home() {
+
+    const [link, setLink] = useState("");
+    const [priority, setPriority] = useState("");
+
+    const handleLink = (e) => {
+        let value = e.target.value;
+        let domain = value.replace('https://', '').replace('http://', '').split('/')[0];
+        setLink(domain);
+        // console.log(getTitle(value).then((res)=>console.log(res)))
+
     }
-    render() {
-        return (
-            <div className="container">
+    // const getTitle = (url) => {
+        
+    //     return fetch(`https://crossorigin.me/${url}`)
+    //         .then((response) => response.text())
+    //         .then((html) => {
+    //             const doc = new DOMParser().parseFromString(html, "text/html");
+    //             const title = doc.querySelectorAll('title')[0];
+    //            return title.innerText;
+    //         });
+    // }
 
-                <div className="row">
-                    <div className="col-25">
-                        <label for="fname">Paste Link</label>
-                    </div>
-                    <div className="col-75">
-                        <input type="text" id="fname" name="firstname" placeholder="Paste Here"></input>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-25">
-                        <label>Priority</label>
-                    </div>
-                    <div className="col-75">
-                        <select id="country" name="country">
-                            <option value="low">Low</option>
-                            <option value="medium">Medium</option>
-                            <option value="high">High</option>
-                        </select>
-                    </div>
-                </div>
-                <div className="row">
-                    <input type="button" value="ADD" placeholder='ADD'></input>
-                </div>
+    const handlePriority = (e) => {
+        let value = e.target.value;
+        setPriority(value)
+    }
 
+    const handleSubmit = () => {
+        // Post data to firebase or any DB
+        console.log('data', { 'Link': link, 'Priority': priority })
+
+
+    }
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-25">
+                    <label htmlFor="fname">Paste Link</label>
+                </div>
+                <div className="col-75">
+                    <input type="text" id="fname" name="firstname" placeholder="Paste Here" onChange={(e) => handleLink(e)}></input>
+                </div>
             </div>
+            <div className="row">
+                <div className="col-25">
+                    <label>Priority</label>
+                </div>
+                <div className="col-75">
+                    <select id="country" name="country" onChange={(e) => handlePriority(e)}>
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </div>
+            </div>
+            <div className="row">
+                <input type="button" value="ADD" placeholder='ADD' onClick={handleSubmit}></input>
+            </div>
+        </div>
 
-        )
-    }
-
+    )
 }
-
 export default Home;
